c3294c503ef45320b076299eccc16cdf55f97baa
ashwanipathak22634 <ashwanipathak22634@gmail.com>
2025-10-24 21:31:39 +0530

    Added mappings for referrals and rewards along with owner initialization in ReferralReward contract

--- contracts/main.sol
+++ contracts/main.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.20;
 
 contract ReferralReward {
-    // Contract will handle user referrals and reward distribution
+    address public owner;
+
+    mapping(address => address) public referrerOf; // who referred whom
+    mapping(address => uint256) public rewards;    // user rewards
+
+    uint256 public rewardAmount = 0.01 ether;      // default reward
+
+    constructor() {
+        owner = msg.sender;
+    }
 }
